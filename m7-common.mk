8e629b62cb5e3e6c7b8a92afa209a3000181f669
Whitehawkx <xxwhitehawk@gmail.com>
2013-09-23 17:43:31 -0700

    m7-common: enable SELinux
    Change-Id: I1a786cdd32caaa856b37accdd531bd4ee4e6eb59
    Signed-off-by: Whitehawkx <xxwhitehawk@gmail.com>

--- m7-common.mk
+++ m7-common.mk
@@ -182,6 +182,10 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
 PRODUCT_AAPT_PREF_CONFIG := xhdpi xxhdpi
 
+# selinux
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.selinux=1 \
+
 # call dalvik heap config
 $(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
 
