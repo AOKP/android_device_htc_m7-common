c98cfcde62667a18c50a461b7e3b374223707f98
David Hays <dhays90@gmail.com>
2013-10-27 08:20:03 -0500

    m7-common: Add m7vzw support
    Change-Id: Id1c2239fe32fadf87fdecde2778762402dd79336

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter m7att m7tmo m7ul,$(TARGET_DEVICE)),)
+ifneq ($(filter m7att m7tmo m7ul m7vzw,$(TARGET_DEVICE)),)
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= fstab.qcom
 LOCAL_MODULE_TAGS	:= optional eng
@@ -9,6 +9,15 @@ LOCAL_SRC_FILES		:= etc/fstab.qcom.gsm
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
+ifeq ($(TARGET_DEVICE),m7vzw)
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= init.target.rc
+LOCAL_MODULE_TAGS	:= optional eng
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= etc/init.target.rc.vzw
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+else
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.target.rc
 LOCAL_MODULE_TAGS	:= optional eng
@@ -16,6 +25,7 @@ LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/init.target.rc.gsm
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+endif
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= remount.qcom
