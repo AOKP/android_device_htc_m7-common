e269797c1d377f3e0bd2144ed25c99b7db11e958
Brandon McAnsh <brandon.mcansh@gmail.com>
2014-02-10 21:29:26 +0100

    m7: Commonize init.target.rc
    * Start services based on ro.build.device (until totally unified)
    
    PS2: remove whitespace
    PS3: Trim filter to m7 (it's our only target besides m7spr and m7vzw)
    PS4: m7vzw uses same fstab as generic m7
    PS5: Fix tabs
    
    Change-Id: I2f2c40fed567266da8cef3e9aa7d5399b2ebcabe
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,37 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter m7 m7att m7tmo m7ul,$(TARGET_DEVICE)),)
+ifneq ($(filter m7 m7vzw,$(TARGET_DEVICE)),)
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= fstab.qcom
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/fstab.qcom.gsm
-LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.target.rc
-LOCAL_MODULE_TAGS	:= optional eng
-LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.target.rc.gsm
-LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-endif
-
-ifeq ($(TARGET_DEVICE),m7vzw)
-include $(CLEAR_VARS)
-LOCAL_MODULE		:= fstab.qcom
-LOCAL_MODULE_TAGS	:= optional eng
-LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/fstab.qcom.gsm
-LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.target.rc
-LOCAL_MODULE_TAGS	:= optional eng
-LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.target.rc.vzw
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 endif
@@ -44,15 +18,15 @@ LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/fstab.qcom.spr
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+endif
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.target.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= etc/init.target.rc.spr
+LOCAL_SRC_FILES		:= etc/init.target.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
-endif
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.qcom.usb.rc
